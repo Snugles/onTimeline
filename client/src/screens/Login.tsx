7df2398e80c644d61933c3f347d0f17ca6d60de6
snuglyWugly <peter.r.ackroyd@gmail.com>
2021-04-15 19:13:30 +0100

    feat:initial commit for client

---
+++ client/src/screens/Login.tsx
@@ -0,0 +1,67 @@
+import React, {useState} from 'react';
+import './styles/Login.css'
+
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+  const [loginMode, setLoginMode] = useState(true);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleChange = (e:any) => {
+    if (e.target.name==='password'){
+      setPassword(e.target.value);
+    } else if (e.target.name==='username'){
+      setUsername(e.target.value);
+    } else {
+      setPasswordConfirm(e.target.value);
+    }
+  }
+
+  const handleSubmit = async (e:any) => {
+    e.preventDefault();
+    if (loginMode) {
+
+      if (!password.length||!username.length) return setErrorMessage('Please fill all fields');
+
+      //login service
+    } else {
+
+      if (password!==passwordConfirm) return setErrorMessage('Passwords do not match');
+      if (!password.length||!username.length) return setErrorMessage('Please fill all fields');
+      
+      //register sevice
+    }
+  }
+  return (
+    <div className="LoginContainer">
+      {loginMode?
+        <form onSubmit = {handleSubmit} className="loginForm">
+          <h1>Login</h1>
+          <p>{errorMessage}</p>
+          <label className="loginLabel">Username:</label>
+            <input type="text" name="username" value={username} onChange={handleChange} className="loginInput"/>
+          <label>Password:</label>
+            <input type="password" name="password" value={password} onChange={handleChange} className="loginInput"/>
+          <input type="submit" value="Submit" className="loginButton"/>
+          <button onClick={()=>setLoginMode(!loginMode)}>I dont have an account, register me</button>
+        </form>:
+
+        <form onSubmit = {handleSubmit} className="loginForm">
+          <h1>Register New Account</h1>
+          <p>{errorMessage}</p>
+          <label className="loginLabel">Username:</label>
+            <input type="text" name="username" value={username} onChange={handleChange} className="loginInput"/>
+          <label>Password:</label>
+            <input type="password" name="password" value={password} onChange={handleChange} className="loginInput"/>
+          <label>Confirm Password:</label>
+            <input type="password" name="passwordConfirm" value={passwordConfirm} onChange={handleChange} className="loginInput"/>
+          <input type="submit" value="Submit" className="loginButton"/>
+          <button onClick={()=>setLoginMode(!loginMode)}>I already have an account, let me log in</button>
+        </form>
+      }
+    </div>
+  );
+}
+
+export default Login;
