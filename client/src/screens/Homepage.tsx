856e6823f08a1e83614d119b2e65a448c7d25141
snuglyWugly <peter.r.ackroyd@gmail.com>
2021-05-01 20:05:11 +0100

    feat: can now make timelines from client

--- client/src/screens/Homepage.tsx
+++ client/src/screens/Homepage.tsx
@@ -3,8 +3,9 @@ import service from '../service';
 import './styles/Homepage.css';
 import timelineInterface from '../interfaces/timeline';
 
-function Login() {
+function Homepage() {
   const [userTimelines, setUserTimelines] = useState([{name:'',id:''}]);
+  const [newName, setNewName] = useState('');
 
   useEffect(()=>{
     service.getTimelines({user_id:1})
@@ -15,11 +16,35 @@ function Login() {
     console.log(id);
   }
 
+  const handleSubmit = (e:any) => {
+    e.preventDefault();
+    service.createTimeline({name:newName, user_id:'1'})
+    .then((res:any)=>{
+      if (res) {
+        console.log(res.name);
+      } else {
+        console.error('error');
+      }
+    })
+    .catch((e:string) => console.error(e));
+  }
+
+  const handleChange = (e:any) => {
+    setNewName(e.target.value);
+  }
+
   return (
     <div className="HomeContainer">
-      {userTimelines.map((timeline:timelineInterface)=><button onClick={()=>loadTimeline(timeline.id)}>{timeline.name}</button>)}
+      {userTimelines.map((timeline:timelineInterface) =>
+        <button className="HomeTimelineButton" onClick={() => loadTimeline(timeline.id)}>{timeline.name}</button>
+      )}
+        <form onSubmit = {handleSubmit} className='homeForm'>
+          <label>Title:</label>
+          <input type="text" name="name" value={newName} onChange={handleChange} className="loginInput"/>
+          <input type="submit" value="Create Topic"/>
+        </form>
     </div>
   );
 }
 
-export default Login;
+export default Homepage;
