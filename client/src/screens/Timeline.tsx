ddd11ce55a0b691f872f983d944bb23362b263a8
snuglyWugly <peter.r.ackroyd@gmail.com>
2021-05-14 21:29:07 +0100

    feat:added timestamps to timeline

--- client/src/screens/Timeline.tsx
+++ client/src/screens/Timeline.tsx
@@ -5,6 +5,7 @@ import './styles/Timeline.css';
 
 function Timeline() {
   const [events, setEvents] = useState([{name:'',id:'',info:'',timeline_date:0, screenpos:0}]);
+  const [dateStamps, setDateStamps] = useState([new Date()]);
   
   const positionEvents = (sortedEvents:any[]) => {
     const dateDifference:number = sortedEvents[sortedEvents.length-1].timeline_date - sortedEvents[0].timeline_date;
@@ -12,6 +13,7 @@ function Timeline() {
     for (const event of sortedEvents) {
       event.screenpos = 95/dateDifference*(event.timeline_date-sortedEvents[0].timeline_date);
     }
+    
     return sortedEvents;
   }
 
@@ -22,11 +24,21 @@ function Timeline() {
           return a.timeline_date - b.timeline_date;
         })
         setEvents(positionEvents(sortedEvents));
+        const dateDifference:number = sortedEvents[sortedEvents.length-1].timeline_date - sortedEvents[0].timeline_date;
+        const output=[];
+        for (let i=0;i<=10;i++) {
+          output.push(new Date(Math.floor(sortedEvents[0].timeline_date+i/10*(dateDifference))))
+        }
+        setDateStamps(output);
       });
   },[]);
 
   return (
     <div className="TimelineContainer">
+      <div className="TimelineDateStamps">
+        {dateStamps?dateStamps.map(date=><p>{date.getHours().toString()+':'+date.getMinutes().toString()+':'+date.getMilliseconds().toString()}</p>):<div>Loading</div>}
+      </div>
+      <div className="TimelineTimeline"/>
       {events&&events.length?
         events.map(element=>
           <Event
@@ -37,7 +49,6 @@ function Timeline() {
             timelinePosition={element.screenpos}/>)
         :<p>No events</p>}
       <button onClick={()=>setEvents(positionEvents(events))}>click me</button>
-      <div className="TimelineTimeline"/>
     </div>
   );
 }
